c3e372417cfc996721d89ae1df098b057009aa46
mariana wismann <marianawis021@gmail.com>
2024-07-03 16:27:24 -0500

    Iniciando el proyecto

---
+++ src/main/java/com/example/demo/Lp2EfWismannMarianaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfWismannMarianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfWismannMarianaApplication.class, args);
+	}
+
+}
